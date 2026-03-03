3e4f6b4511876248134ee1eb339484939d6b3981
Shivangi Dhole <dholeshivangi06@gmail.com>
2026-03-03 10:58:00 +0530

    this is UC 2

---
+++ OOPSBannerApp2.java
@@ -0,0 +1,23 @@
+/**
+ * OOPSBannerApp2 UC2 – Banner Display Application
+ * This class displays the word "OOPS" in a banner format
+ * using asterisks (*) and spaces.
+ *
+ * @author Shivangi Dhole
+ * @version 2.0
+ */
+public class OOPSBannerApp2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME\n");
+
+        System.out.println("  *****     *****     ******     ******  ");
+        System.out.println(" **   **   **   **   **    **   **        ");
+        System.out.println(" **   **   **   **   **    **   **        ");
+        System.out.println(" **   **   **   **   ******      *****   ");
+        System.out.println(" **   **   **   **   **                 ** ");
+        System.out.println(" **   **   **   **   **          **    ** ");
+        System.out.println("  *****     *****     **           ****** ");
+    }
+}
